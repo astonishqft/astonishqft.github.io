d67ed9bd2b98a09a635265c5a85f7b5e872b6536
qi.futao <qi.futao@iwhalecloud.com>
2021-10-19 21:02:20 +0800

    init

--- .umirc.js
+++ .umirc.js
@@ -1,32 +1,12 @@
+import { defineConfig } from 'umi';
 
-// ref: https://umijs.org/config/
-export default {
-  treeShaking: true,
-  history: 'hash',
-  "theme": {
-    "primary-color": "#1DA57A",
+export default defineConfig({
+  history: {
+    type: 'hash',
   },
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'astonishqft.github.io',
-      dll: true,
-      locale: {
-        enable: true,
-        default: 'en-US',
-      },
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
-}
+  theme: {
+    "@primary-color": "#1DA57A",
+  },
+  antd: {},
+  dva: {},
+});
